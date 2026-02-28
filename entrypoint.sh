2df7a07f0a06a0010a3ef7ab5e5b600df59e31de
Yanis Wilbrand <yanis.wilbrand@gmail.com>
2026-02-28 20:16:40 +0100

    included entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e
 
+SCRIPT="Scripts/adversarial_tts_harvard.py"
+REMAINING_ARGS=()
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --script) SCRIPT="$2"; shift 2 ;;
+        *) REMAINING_ARGS+=("$1"); shift ;;
+    esac
+done
+
 echo "Cloning latest code from GitHub..."
 git clone https://github.com/Vorgesetzter/StyleTTS2 /app/code
 
@@ -8,4 +17,4 @@ git clone https://github.com/Vorgesetzter/StyleTTS2 /app/code
 ln -s /app/Audio /app/code/Audio
 
 cd /app/code
-python -u Scripts/adversarial_tts_harvard.py "$@"
+python -u "$SCRIPT" "${REMAINING_ARGS[@]}"
